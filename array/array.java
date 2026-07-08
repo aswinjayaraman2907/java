cd7bfd0ecba96ec275a62ee3ba279d7b780a2ac6
ASWIN J <aswinjayaraman2907@gmail.com>
2026-07-08 10:50:12 +0530

    Add files via upload

---
+++ array/array.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class array {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int arr[] =new int[5];
+        System.out.println("Enter the number");
+        
+    }
+    
+}
